971845097dc239a20e451e2fe80816f76a942a5b
fkdakt0226 <fkdakt0226@gmail.com>
2024-03-24 04:23:29 +0900

    OpenAI APIを利用するための設定
    利用自体はまだ未実装

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from voicevox_core import VoicevoxCore, METAS
+from openai import OpenAI
 import sys, os
+import config
+
+openai_api_key = config.OPENAI_API_KEY
 
 core = VoicevoxCore(open_jtalk_dict_dir=Path("./open_jtalk_dic_utf_8-1.11"))
 speaker_id = 1
@@ -9,5 +13,5 @@ text = input(str)
 if not core.is_model_loaded(speaker_id):
     core.load_model(speaker_id)
 wave_bytes = core.tts(text, speaker_id)
-with open("./" + text + ".wav", "wb") as f:
+with open("./output.wav", "wb") as f:
     f.write(wave_bytes)
